e6837071714ad2bffef46fcf1d41f7f139021528
Abhay Mahendera <abhaymahendera0@gmail.com>
2025-03-27 16:36:49 -0400

    Sample code for login / signup

---
+++ main.py
@@ -0,0 +1,22 @@
+import utils
+import auth
+
+def main():
+        print("|--------------------------------|")
+        print("| Welcome to Fitness Tracker App |")
+        print("|--------------------------------|")
+        
+        choice = input("1. Login\n2. Register\n3. Exit\nChoice: ").strip()
+
+        match choice:
+            case "1":
+                auth.login()
+            case "2":
+                auth.make_new_user() 
+            case "3":
+                exit()
+            case _:
+                print("Invalid choice!")
+
+if __name__ == "__main__":
+    main()
